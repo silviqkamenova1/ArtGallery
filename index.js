ef68d83e1d462b7ec3bc206a22a821c37a6ff57a
silviqkamenova1 <117118888+silviqkamenova1@users.noreply.github.com>
2023-09-19 21:15:06 +0300

    add

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const handlebars = require('express-handlebars');
-const cookieParser = require('cookie-parser');
-const mongoose = require('mongoose')
+//const cookieParser = require('cookie-parser');
+//const mongoose = require('mongoose')
 
 const routes = require('./routes');
-const {authentication} = require('./middlewares/authMiddleware');
+//const {authentication} = require('./middlewares/authMiddleware');
 const app = express();
 
 app.engine('hbs', handlebars.engine({
@@ -16,11 +16,11 @@ app.set('view engine', 'hbs')
 app.use('/static',express.static('public'))
 app.use(express.urlencoded({extended: false}))//to parse data from sended forms with post request
 //which will be parsed and recived in req.body like object
-app.use(cookieParser());
-app.use(authentication)
+//app.use(cookieParser());
+//app.use(authentication)
 app.use(routes);
 
-mongoose.set('strictQuery', false);
-mongoose.connect('mongodb://127.0.0.1:27017/art')
+// mongoose.set('strictQuery', false);
+// mongoose.connect('mongodb://127.0.0.1:27017/art')
 
-app.listen(3000, () => 'Server is running on port 3000...')
+app.listen(5000, () => 'Server is running on port 5000...')
